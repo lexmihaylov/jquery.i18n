1f57bdf1957963c1dd05309d1fc90ad750391267
alexander <alexander@alexander-158.(none)>
2013-11-14 19:53:58 +0200

    first commit

---
+++ lang.js
@@ -0,0 +1,24 @@
+$.i18n.languages = {
+    'en': {
+        'title': 'Example use of i18n.',
+        'hello': 'Hello, ',
+        'world': 'world!',
+        'button': {
+            'en': 'English',
+            'bg': 'Bulgarian',
+            'add_dynamic_content': 'Add dynamic content'
+        },
+        'enter_content': 'Enter text'
+    },
+    'bg': {
+        'title': 'Пример за използване на i18n',
+        'hello': 'Здравей, ',
+        'world': 'свят!',
+        'button': {
+            'en': 'Английски',
+            'bg': 'Български',
+            'add_dynamic_content': 'Добави динамично съдържание'
+        },
+        'enter_content': 'Въведете текст'
+    }
+};
